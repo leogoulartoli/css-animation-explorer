142f2e8259af42597c5c958569aa878a586f9504
Leonardo Goulart de Oliveira <leogoulartoli@gmail.com>
2022-11-18 12:31:36 -0300

    Finished Transforms2D

--- src/components/Transforms2D/index.tsx
+++ src/components/Transforms2D/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styles from "./style.module.css";
 
 export const Transforms2D = () => {
-  return <div>index</div>;
+  return (
+    <div id={styles.container}>
+      <button className={styles.move}>Translate</button>
+      <button className={styles.scale}>Scale</button>
+      <button className={styles.rotate}>Rotate</button>
+      <button className={styles.rotateOrigin}>Rotate Origin</button>
+      <button className={styles.skew}>Skew</button>;
+    </div>
+  );
 };
