1b2b107d64afc2259eb30a65f7c1680915d32c5a
Leonardo Goulart de Oliveira <leogoulartoli@gmail.com>
2022-11-19 16:40:35 -0300

    Add opacity button effect

---
+++ src/components/ButtonEffects/HoverOpacity/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./style.module.css";
+
+export const HoverOpacity = () => {
+  return <button className={`${styles.button}`}>Opacity</button>;
+};
--- src/components/ButtonEffects/index.tsx
+++ src/components/ButtonEffects/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { IoMdMoon, IoMdSunny } from "react-icons/io";
+import { HoverOpacity } from "./HoverOpacity";
 import { HoverDegree } from "./HoverDegree";
 import { HoverDuplicate } from "./HoverDuplicate";
 import { HoverGlow } from "./HoverGlow";
@@ -16,7 +17,7 @@ export const ButtonEffects = () => {
         className="w-20 h-20 cursor-pointer"
         onClick={() => setShowDarkEffects(false)}
       />
-      <div className="w-full flex justify-center items-center gap-7 h-screen bg-black">
+      <div className="w-full flex justify-center items-center gap-7 px-72 py-7 bg-black">
         <HoverGlow />
       </div>
     </>
@@ -26,12 +27,13 @@ export const ButtonEffects = () => {
         className="w-20 h-20 cursor-pointer"
         onClick={() => setShowDarkEffects(true)}
       />
-      <div className="w-full h-screen flex justify-center items-center gap-7">
+      <div className="w-full flex justify-center flex-wrap items-center gap-7 py-7 px-72">
         <HoverDegree />
         <HoverMove />
         <HoverRotate />
         <HoverDuplicate />
         <Stretching />
+        <HoverOpacity />
       </div>
     </>
   );
